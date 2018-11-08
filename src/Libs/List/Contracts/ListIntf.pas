436c1914eb06d805fdd1325eb0bfac8551be8e5a
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-08 09:27:42 +0800

    add missing files

---
+++ src/Libs/List/Contracts/ListIntf.pas
@@ -0,0 +1,32 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (GPL 3.0)
+ *------------------------------------------------------------- *)
+
+unit ListIntf;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+    {------------------------------------------------
+     interface for any class having capability store hash list
+     @author Zamrony P. Juhara <zamronypj@yahoo.com>
+    -----------------------------------------------}
+    IList = interface
+        ['{0F439A0A-7156-4DC6-9B8C-96F4439810E7}']
+        function count() : integer;
+        function get(const indx : integer) : pointer;
+        procedure delete(const indx : integer);
+        function add(const key : shortstring; const routeData : pointer) : integer;
+        function find(const key : shortstring) : pointer;
+        function keyOfIndex(const indx : integer) : shortstring;
+    end;
+
+implementation
+end.
---
+++ src/Libs/List/Implementations/HashListImpl.pas
@@ -0,0 +1,83 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit HashListImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+    contnrs,
+    ListIntf;
+
+type
+    {------------------------------------------------
+     interface for any class having capability to store
+     hash list
+     @author Zamrony P. Juhara <zamronypj@yahoo.com>
+    -----------------------------------------------}
+    THashList = class(TInterfacedObject, IList)
+    private
+        hashes : TFPHashList;
+    public
+        constructor create();
+        destructor destroy(); override;
+
+        function count() : integer;
+        function get(const indx : integer) : pointer;
+        procedure delete(const indx : integer);
+        function add(const key : shortstring; const data : pointer) : integer;
+        function find(const key : shortstring) : pointer;
+        function keyOfIndex(const indx : integer) : shortstring;
+    end;
+
+implementation
+
+    constructor THashList.create();
+    begin
+        hashes := TFPHashList.create();
+    end;
+
+    destructor THashList.destroy();
+    begin
+        inherited destroy();
+        hashes.free();
+    end;
+
+    function THashList.count() : integer;
+    begin
+        result := hashes.count;
+    end;
+
+    function THashList.get(const indx : integer) : pointer;
+    begin
+        result := hashes.items[indx];
+    end;
+
+    procedure THashList.delete(const indx : integer);
+    begin
+        hashes.delete(indx);
+    end;
+
+    function THashList.add(const key : shortstring; const data : pointer) : integer;
+    begin
+        result := hashes.add(key, data);
+    end;
+
+    function THashList.find(const key : shortstring) : pointer;
+    begin
+        result := hashes.find(key);
+    end;
+
+    function THashList.keyOfIndex(const indx : integer) : shortstring;
+    begin
+        result := hashes.nameOfIndex(indx);
+    end;
+end.
