407866ff8e0f62ce037f568bd65c0b0b8f4b6a24
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-10-20 14:55:45 +0800

    initial implementation of uwsgi project creation

---
+++ src/Tasks/Implementations/ProjectUwsgi/CreateUwsgiAppBootstrapTaskImpl.pas
@@ -0,0 +1,97 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit CreateUwsgiAppBootstrapTaskImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    TaskOptionsIntf,
+    TaskIntf,
+    CreateAppBootstrapTaskImpl;
+
+type
+
+    (*!--------------------------------------
+     * Task that create uwsgi web application project
+     * application bootstrapper using Fano Framework
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TCreateScgiAppBootstrapTask = class(TCreateAppBootstrapTask)
+    private
+        function getHost(const opt : ITaskOptions) : string;
+        function getPort(const opt : ITaskOptions) : string;
+    protected
+        procedure createApp(
+            const opt : ITaskOptions;
+            const longOpt : shortstring;
+            const dir : string
+        ); override;
+
+        procedure createBootstrap(
+            const opt : ITaskOptions;
+            const longOpt : shortstring;
+            const dir : string
+        ); override;
+    end;
+
+implementation
+
+uses
+
+    sysutils;
+
+    function TCreateUwsgiAppBootstrapTask.getHost(const opt : ITaskOptions) : string;
+    begin
+        //TODO: refactor as this is exactly same as getHost() in TBaseApacheVirtualHostTask
+        result := opt.getOptionValue('host');
+        if result = '' then
+        begin
+            result := '127.0.0.1';
+        end;
+    end;
+
+    function TCreateUwsgiAppBootstrapTask.getPort(const opt : ITaskOptions) : string;
+    begin
+        //TODO: refactor as this is exactly same as getHost() in TBaseApacheVirtualHostTask
+        result := opt.getOptionValue('port');
+        if result = '' then
+        begin
+            result := '20477';
+        end;
+    end;
+
+    procedure TCreateUwsgiAppBootstrapTask.createApp(
+        const opt : ITaskOptions;
+        const longOpt : shortstring;
+        const dir : string
+    );
+    var
+        {$INCLUDE src/Tasks/Implementations/ProjectUwsgi/Includes/app.pas.inc}
+    begin
+        fContentModifier.setVar('[[HOST]]', getHost(opt));
+        fContentModifier.setVar('[[PORT]]', getPort(opt));
+        createTextFile(dir + '/app.pas', fContentModifier.modify(strAppPas));
+    end;
+
+    procedure TCreateUwsgiAppBootstrapTask.createBootstrap(
+        const opt : ITaskOptions;
+        const longOpt : shortstring;
+        const dir : string
+    );
+    var
+        {$INCLUDE src/Tasks/Implementations/ProjectUswsgi/Includes/bootstrap.pas.inc}
+    begin
+        createTextFile(dir + '/bootstrap.pas', strBootstrapPas);
+    end;
+
+end.
---
+++ src/Tasks/Implementations/ProjectUwsgi/Factories/CreateProjectUwsgiTaskFactoryImpl.pas
@@ -0,0 +1,68 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit CreateProjectUwsgiTaskFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    TaskIntf,
+    TaskFactoryIntf,
+    TextFileCreatorIntf,
+    ContentModifierIntf,
+    CreateProjectTaskFactoryImpl;
+
+type
+
+    (*!--------------------------------------
+     * Factory class for create project task for
+     * FastCGI web application
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TCreateProjectUwsgiTaskFactory = class(TCreateProjectTaskFactory)
+    protected
+        function buildProjectTask(
+            const textFileCreator : ITextFileCreator;
+            const contentModifier : IContentModifier
+        ) : ITask; override;
+    end;
+
+implementation
+
+uses
+
+    DirectoryCreatorImpl,
+    InitGitRepoTaskImpl,
+    CommitGitRepoTaskImpl,
+    CreateDirTaskImpl,
+    CreateAppConfigsTaskImpl,
+    CreateAdditionalFilesTaskImpl,
+    CreateShellScriptsTaskImpl,
+    CreateUwsgiAppBootstrapTaskImpl,
+    CreateProjectTaskImpl;
+
+    function TCreateProjectUwsgiTaskFactory.buildProjectTask(
+        const textFileCreator : ITextFileCreator;
+        const contentModifier : IContentModifier
+    ) : ITask;
+    begin
+        result := TCreateProjectTask.create(
+            TCreateDirTask.create(TDirectoryCreator.create()),
+            TCreateShellScriptsTask.create(textFileCreator, contentModifier, 'bin'),
+            TCreateAppConfigsTask.create(textFileCreator, contentModifier),
+            TCreateAdditionalFilesTask.create(textFileCreator, contentModifier),
+            TCreateUwsgiAppBootstrapTask.create(textFileCreator, contentModifier),
+            TInitGitRepoTask.create(TCommitGitRepoTask.create())
+        );
+    end;
+
+end.
