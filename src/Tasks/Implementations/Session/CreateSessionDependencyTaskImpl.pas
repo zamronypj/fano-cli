aa88943c6dae43ed76dcecc4dcdae00c9750e496
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-11-26 09:53:51 +0800

    initial implementation of session support implementation

---
+++ src/Tasks/Implementations/Session/CreateSessionDependencyTaskImpl.pas
@@ -0,0 +1,84 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit CreateSessionDependenciesTaskImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    TaskOptionsIntf,
+    TaskIntf,
+    DecoratorTaskImpl;
+
+type
+
+    (*!--------------------------------------
+     * Task that add session support to project creation
+     *---------------------------------------------
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TCreateSessionDependenciesTask = class(TDecoratorTask)
+    private
+        createFileSessionDependenciesTask : ITask;
+        createCookieSessionDependenciesTask : ITask;
+        createDbSessionDependenciesTask : ITask;
+    public
+        constructor create(
+            const createFileSessDependenciesTask : ITask;
+            const createCookieSessDependenciesTask : ITask;
+            const createDbSessDependenciesTask : ITask
+        );
+        destructor destroy(); override;
+        function run(
+            const opt : ITaskOptions;
+            const longOpt : shortstring
+        ) : ITask; override;
+    end;
+
+implementation
+
+    constructor TCreateSessionDependenciesTask.create(
+        const createFileSessDependenciesTask : ITask;
+        const createCookieSessDependenciesTask : ITask;
+        const createDbSessDependenciesTask : ITask
+    );
+    begin
+        createFileSessionDependenciesTask := createFileSessDependenciesTask;
+        createCookieSessionDependenciesTask := createCookieSessDependenciesTask;
+        createDbSessionDependenciesTask := createDbSessDependenciesTask;
+    end;
+
+    destructor TCreateSessionDependenciesTask.destroy();
+    begin
+        createFileSessionDependenciesTask := nil;
+        createCookieSessionDependenciesTask := nil;
+        createDbSessionDependenciesTask := nil;
+        inherited destroy();
+    end;
+
+    function TCreateSessionDependenciesTask.run(
+        const opt : ITaskOptions;
+        const longOpt : shortstring
+    ) : ITask;
+    var sessType : string;
+    begin
+        if opt.has('with-session') then
+        begin
+            sessType := opt.getOptionValueDef('with-session', 'file');
+            case sessType of
+                'file' : result := createFileSessionDependenciesTask.run(opt,longOpt);
+                'cookie' : result := createCookieSessionDependenciesTask.run(opt,longOpt);
+                'db' : result := createDbSessionDependenciesTask.run(opt,longOpt);
+            end;
+        end;
+        result := self;
+    end;
+end.
