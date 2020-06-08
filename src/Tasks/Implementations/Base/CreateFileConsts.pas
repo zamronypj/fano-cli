a0a179cd093fd340003970c0ac9b12b4e6f840b5
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-06-08 10:51:03 +0800

    add missing file

---
+++ src/Tasks/Implementations/Base/CreateFileConsts.pas
@@ -0,0 +1,18 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 - 2020 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit CreateFileConsts;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+const BASE_DIRECTORY = 'src' + DirectorySeparator + 'App';
+
+implementation
+end.
