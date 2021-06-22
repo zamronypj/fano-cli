ead9ffe3a74e4884071b860e01c9ee617e959992
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-06-22 07:54:33 +0700

    add capability to isplay single task info

---
+++ src/Tasks/Implementations/Info/SingleInfoTaskImpl.pas
@@ -0,0 +1,93 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 - 2020 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit SingleInfoTaskImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    ListIntf,
+    TaskOptionsIntf,
+    TaskIntf;
+
+type
+
+    (*!--------------------------------------
+     * Task that display help information
+     * for single task
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TSingleInfoTask = class(TInterfacedObject, ITask)
+    private
+        fTaskList : IList;
+    public
+        constructor create(const tasks : IList);
+        function run(
+            const opt : ITaskOptions;
+            const longOpt : shortstring
+        ) : ITask;
+    end;
+
+implementation
+
+uses
+
+    TaskItemTypes;
+
+    constructor TSingleInfoTask.create(const tasks : IList);
+    begin
+        fTaskList := tasks;
+    end;
+
+    function findTask(const taskList : IList; const taskName : string) : PTaskItem;
+    var i, taskCount : integer;
+        item : PTaskItem;
+    begin
+        result := nil;
+        taskCount := taskList.count();
+        for i:=0 to taskCount-1 do
+        begin
+            item := taskList.get(i);
+            if taskName = item^.longOption then
+            begin
+                result := item;
+                exit;
+            end;
+        end;
+    end;
+
+    function TSingleInfoTask.run(
+        const opt : ITaskOptions;
+        const longOpt : shortstring
+    ) : ITask;
+    var taskName : string;
+        taskItem : PTaskItem;
+    begin
+        taskName := opt.getOptionValue('task');
+        taskItem := findTask(fTaskList, taskName);
+
+        writeln('Fano CLI, utility for Fano Framework');
+        writeln('https://fanoframework.github.io');
+        writeln();
+
+        if (taskItem = nil) then
+        begin
+            writeln('Task ' + taskName + ' is not found.');
+        end else
+        begin
+            writeln('Usage: fanocli --' + taskName + ' [Task Parameters]');
+            writeln();
+            writeln('   ', taskItem^.description);
+        end;
+        result := self;
+    end;
+end.
---
+++ src/Tasks/Implementations/Info/WithSingleInfoTaskImpl.pas
@@ -0,0 +1,46 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 - 2020 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit WithSingleInfoTaskImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    TaskOptionsIntf,
+    TaskIntf,
+    ConditionalCompositeTaskImpl;
+
+type
+
+    (*!--------------------------------------
+     * Task that execute first task if --task
+     * is set, otherwise run second task
+     *---------------------------------------------
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TWithSingleInfoTask = class(TConditionalCompositeTask)
+    protected
+        function condition(
+            const opt : ITaskOptions;
+            const longOpt : shortstring
+        ) : boolean; override;
+    end;
+
+implementation
+
+    function TWithSingleInfoTask.condition(
+        const opt : ITaskOptions;
+        const longOpt : shortstring
+    ) : boolean;
+    begin
+        result := opt.hasOption('task');
+    end;
+end.
