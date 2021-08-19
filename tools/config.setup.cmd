2b29306d764cdc853f46e418bbdd555928afd351
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-08-19 19:22:17 +0700

    improve windows uspport

--- tools/config.setup.cmd
+++ tools/config.setup.cmd
@@ -1,14 +1,15 @@
-REM------------------------------------------------------------
+@echo off
+REM ------------------------------------------------------------
 REM Fano CLI Application (https://fanoframework.github.io)
 REM
 REM @link      https://github.com/fanoframework/fano-cli
 REM @copyright Copyright (c) 2018 Zamrony P. Juhara
 REM @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
-REM-------------------------------------------------------------
+REM -------------------------------------------------------------
 
-REM------------------------------------------------------
+REM ------------------------------------------------------
 REM Scripts to setup configuration files
-REM------------------------------------------------------
+REM ------------------------------------------------------
 
 copy build.prod.cfg.sample build.prod.cfg
 copy build.dev.cfg.sample build.dev.cfg
