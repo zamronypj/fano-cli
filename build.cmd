487915ad7bede8011ddb541108ef7c58ab28e63f
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-08-19 18:46:41 +0700

    initial implementation windows support

--- build.cmd
+++ build.cmd
@@ -1,10 +1,11 @@
-REM------------------------------------------------------------
+@echo off
+REM ------------------------------------------------------------
 REM Fano CLI Application (https://fanoframework.github.io)
 REM
 REM @link      https://github.com/fanoframework/fano-cli
 REM @copyright Copyright (c) 2018 Zamrony P. Juhara
 REM @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
-REM-------------------------------------------------------------
+REM -------------------------------------------------------------
 
 REM ------------------------------------
 REM -- build script for Windows
@@ -14,8 +15,8 @@ IF NOT EXIST build.cfg (copy build.cfg.sample build.cfg)
 IF NOT EXIST build.dev.cfg (copy build.dev.cfg.sample build.dev.cfg)
 IF NOT EXIST build.prod.cfg (copy build.prod.cfg.sample build.prod.cfg)
 
-IF NOT DEFINED BUILD_TYPE (SET BUILD_TYPE="prod")
-IF NOT DEFINED UNIT_OUTPUT_DIR (SET UNIT_OUTPUT_DIR="bin\unit")
-IF NOT DEFINED EXEC_OUTPUT_DIR (SET EXEC_OUTPUT_DIR="bin\out")
+IF NOT DEFINED BUILD_TYPE (SET BUILD_TYPE=prod)
+IF NOT DEFINED UNIT_OUTPUT_DIR (SET UNIT_OUTPUT_DIR=bin\unit)
+IF NOT DEFINED EXEC_OUTPUT_DIR (SET EXEC_OUTPUT_DIR=bin\out)
 
 fpc @unit.search.cfg @build.cfg src/fanocli.pas
