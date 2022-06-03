9c6f4afff68ba72f9c922945b32f860fa73d02f0
Zamrony P. Juhara <zamronypj@yahoo.com>
2022-06-04 05:06:17 +0700

    initial impleementation of full-featured project scaffold

---
+++ src/Tasks/Implementations/Project/Fancy/Factories/CreateAppBootstrapFancyTaskFactoryImpl.pas
@@ -0,0 +1,66 @@
+(*!------------------------------------------------------------
+ * Fano CLI Application (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano-cli
+ * @copyright Copyright (c) 2018 - 2022 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
+ *------------------------------------------------------------- *)
+unit CreateAppBootstrapFancyTaskFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    TaskIntf,
+    TaskFactoryIntf,
+    TextFileCreatorIntf,
+    ContentModifierIntf,
+    CreateAppBootstrapTaskFactoryImpl;
+
+type
+
+    (*!--------------------------------------
+     * factory class for http application bootstrap task
+     * using TFpHttpServer with Fancy template
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *---------------------------------------*)
+    TCreateAppBootstrapFancyTaskFactory = class(TCreateAppBootstrapTaskFactory)
+    protected
+        function buildBootstrapTask(
+            const textFileCreator : ITextFileCreator;
+            const contentModifier : IContentModifier
+        ) : ITask; override;
+    end;
+
+implementation
+
+uses
+
+    GroupTaskImpl,
+
+    CreateFpwebAppFileTaskImpl,
+    CreateFpwebReadmeFileTaskImpl,
+    CreateDaemonBootstrapFileTaskImpl,
+    CreateDepFileTaskImpl,
+    CreateRouteFileTaskImpl;
+
+
+    function TCreateAppBootstrapFancyTaskFactory.buildBootstrapTask(
+        const textFileCreator : ITextFileCreator;
+        const contentModifier : IContentModifier
+    ) : ITask;
+    begin
+        result := TGroupTask.create([
+            TCreateFpwebAppFileTask.create(textFileCreator, contentModifier),
+            TCreateFpwebReadmeFileTask.create(textFileCreator, contentModifier),
+            TCreateDaemonBootstrapFileTask.create(textFileCreator, contentModifier),
+            TCreateDepFileTask.create(textFileCreator, contentModifier),
+            TCreateRouteFileTask.create(textFileCreator, contentModifier)
+        ]);
+    end;
+
+end.
